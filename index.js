b3d3e395c7dda61435c5e2753cf4eb7a150033f4
Rachael <rachael92njoki@gmail.com>
2023-12-29 11:43:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+let names = ['Rachael', 'John', 'James'];
+
+function writeCards(names, birthday) {
+    const messages=[];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    
+    return messages;
+}
+
+writeCards(names, "birthday");
+//while loop
+
+function countDown(num){
+    
+    while (num >=0){
+        console.log(num--);
+    }
+}
